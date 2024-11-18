194468198e106a420ab13d40d912cd8256693d2d
Gaetz <gaetz@rufflerim.com>
2024-11-18 18:37:41 +0100

    [feature] Download, include and link SDL3

---
+++ Main.cpp
@@ -0,0 +1,13 @@
+//
+// Created by Gaëtan Blaise-Cazalet on 18/11/2024.
+//
+
+#include <iostream>
+#include <SDL3/SDL.h>
+
+using namespace std;
+
+int main() {
+    cout << "Hello, World!" << endl;
+    return 0;
+}
